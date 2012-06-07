280fab3d2838fcab79d5a1f42e872421fada8d1e
hollyormseth <hollyormseth@gmail.com>
2012-06-06 23:16:25 -0400

    tried something

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require './schools'
 
 @schools.each do |school|
-  puts "We love " + school.split(' ').last
+	i = 0 
+	num = school.split(" ").length
+	name = String.new(" ")
+	while i < num do
+		if school.split(' ')[i] != "University" and school.split(' ')[i] != "the" and school.split(' ')[i] !=  "The" and school.split(' ')[i] != "of" and school.split(' ')[i] !=  "College"
+			name += school.split(' ')[i] + " "
+		end
+		i += 1
+	end
+	puts "We love" + name 
 end
