44d086861d445cddca82eae9f7723bcb1b9a9017
hollyormseth <hollyormseth@gmail.com>
2012-06-07 00:44:55 -0400

    betterish

--- app.rb
+++ app.rb
@@ -1,14 +1,5 @@
 require './schools'
-
+backlist = /University |of |College |the |The | University/
 @schools.each do |school|
-	i = 0 
-	num = school.split(" ").length
-	name = String.new(" ")
-	while i < num do
-		if school.split(' ')[i] != "University" and school.split(' ')[i] != "the" and school.split(' ')[i] !=  "The" and school.split(' ')[i] != "of" and school.split(' ')[i] !=  "College"
-			name += school.split(' ')[i] + " "
-		end
-		i += 1
-	end
-	puts "We love" + name 
-end
+	puts "We love " + school.gsub(backlist, "")
+end 
