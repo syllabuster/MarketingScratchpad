1683c869b02c3e327e26ba4350ebbc7ce8dd1559
Matt <matt@primestudiosllc.com>
2012-06-06 18:39:28 -0700

    create test loop

--- app.rb
+++ app.rb
@@ -1,2 +1,6 @@
-puts "mmkay"
+schools = ["Brown University", "The University of Chicago", "MIT", "Dartmouth College", "University of Notre Dame", "UCLA"]
 
+schools.each do |school|
+  #This is where the magic happens
+  puts "We love " + school.split(' ').first
+end
